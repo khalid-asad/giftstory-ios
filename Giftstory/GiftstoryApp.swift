553fa7fbbf9689ca15a5351c2866df531ba4ed3c
Khalid Asad <khalidasad93@gmail.com>
2021-05-02 15:31:19 -0400

    [GFY-0001] Initialize project.

---
+++ Giftstory/GiftstoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftstoryApp.swift
+//  Giftstory
+//
+//  Created by Khalid Asad on 2021-05-02.
+//
+
+import SwiftUI
+
+@main
+struct GiftstoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
